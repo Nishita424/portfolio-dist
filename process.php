3cdfa83f2fff28bfef75568c2619b2de0c2c0fa7
nishitagiduturi <nishitaoct26@gmail.com>
2020-11-10 11:40:03 +0530

    Updated content, added backend for form

---
+++ process.php
@@ -0,0 +1,17 @@
+<?php
+// name attributes 
+    if(isset($_POST['email'])){
+        // Email information
+        $admin_email = "gnishita28@gmail.com";
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $phone = $_POST['phone'];
+        $message = $_POST['message'];
+        $subject = $_POST['subject'];
+        
+        // Send email:(email, subject, body: mesg+phone)
+        mail($admin_email, "New Form Submission", $message .'-'.$phone, "From:".$email);
+        
+        header('Location: https://gni-portfolio.netlify.app/success.html');
+    }
+    
